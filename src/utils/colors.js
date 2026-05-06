c1688eb206937c19b7f1e405512be2e8a2bae7c7
EliasColturato <eliasjuniortksj@hotmail.com>
2026-05-06 10:58:44 -0300

    Routes and color codes
    I created a new file ("appRoutes.jsx") to control the routes on our
    project.
    
    I create the file utils/colors.js to add the code of all colors.
    
    REMOVED:
    
    I removed the files app.jsx and app.css

---
+++ src/utils/colors.js
@@ -0,0 +1,20 @@
+export const primaryColor = {
+  50: '#EDF7EE',
+  100: '#C8E5CB',
+  200: '#ADD8B2',
+  300: '#88C68E',
+  400: '#71BB79',
+  500: '#4DAA57',
+  600: '#469B4F',
+  700: '#37793E',
+  800: '#2A5E30',
+  900: '#204725',
+};
+
+export const textColor = {
+  primary: '#050505',
+  secondary: '#494949',
+  tertiary: '#8F8F8F',
+  disable: '#DADADA',
+  white: '#ffffff',
+};
