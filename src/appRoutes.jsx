5f9201178da1d3d5eb6d63719114d971950a7d08
EliasColturato <eliasjuniortksj@hotmail.com>
2026-05-06 11:05:45 -0300

    New folders and Routes
    I created new folder for assets, components, pages.
    
    The routes file is configured and ready to receive new routes.

--- src/appRoutes.jsx
+++ src/appRoutes.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+
 export default function AppRoutes() {
   return (
-    <>
-      <h1>teste</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
