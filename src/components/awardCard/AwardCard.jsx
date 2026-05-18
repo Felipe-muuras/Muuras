1d9c984e98be1c073251f88c6dae3e65673b1388
EliasColturato <eliasjuniortksj@hotmail.com>
2026-05-18 18:33:36 -0300

    fix award card image paths

--- src/components/awardCard/AwardCard.jsx
+++ src/components/awardCard/AwardCard.jsx
@@ -3,7 +3,7 @@ import { AwardCardContainer, AwardTextCardContainer } from './styleAwardCard';
 export default function AwardCard({ imagePath, title }) {
   return (
     <AwardCardContainer>
-      <img src={`${import.meta.env.BASE_URL}${imagePath}`} alt={title} />
+      <img src={imagePath} alt={title} />
       <AwardTextCardContainer>
         <h2>{title}</h2>
       </AwardTextCardContainer>
